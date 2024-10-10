d97947757c7f178fa792f3074e9f820f97c2624a
Md Shakil Hossin <shakilshd23@gmail.com>
2024-10-11 03:09:37 +0600

    Homepage Desktop Done

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,40 @@
-console.log('shakil')
+$('.besten-angebote-slider').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.sonderangebot-slider').owlCarousel({
+    items: 1,
+    loop:true,
+    nav:true,
+    
+})
+
+$('.nolan-slider').owlCarousel({
+    items: 1,
+    loop:true,
+    nav:false,
+    dots: true,
+    // autoplay: true
+})
+
+$('.rv-marken-slider').owlCarousel({
+    items: 6,
+    loop:true,
+    nav:true,
+    autoplay: true,
+    dots: false
+})
+

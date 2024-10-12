1571acd6ea25d331eba5d45ae718efa315244c88
Md Shakil Hossin <shakilshd23@gmail.com>
2024-10-12 11:02:34 +0600

    mobile menu done

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,41 @@
+// Mobile menu js
+const header = document.getElementById('rv-header')
+const mobileContent = document.getElementById('mobile-content');
+const mobileIcon = document.getElementById('mobile-icon');
+const mobileClose = document.getElementById('close-icon');
+mobileIcon.addEventListener('click',mobileMenuShow);
+mobileClose.addEventListener('click',mobileMenuHide);
+function mobileMenuShow() {
+    header.style.overflow = 'visible';
+    mobileContent.style.transform = 'translate(0)';
+    document.body.classList.add('dark-background');
+    document.body.classList.style.overflow = 'hidden';
+}
+function mobileMenuHide() {
+    setTimeout(() => {header.style.overflow = 'hidden';},300)
+    mobileContent.style.transform = 'translate(100%)';
+    document.body.classList.remove('dark-background');
+    document.body.classList.style.overflow = 'visible';
+}
 
+
+
+// All slider js
 $(document).ready(function () {
 
-    // 
+    $('.mobile-menu-slider').owlCarousel({
+        items: 2,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        margin: 10
+    })
+
     $('.besten-angebote-slider').owlCarousel({
         loop: true,
         margin: 20,
         nav: true,
+        autoplay: true,
         responsive: {
             320: {
                 items: 1
@@ -32,7 +62,7 @@ $(document).ready(function () {
         items: 1,
         loop: true,
         nav: true,
-
+        autoplay: true,
     })
 
     $('.nolan-slider').owlCarousel({
@@ -40,7 +70,7 @@ $(document).ready(function () {
         loop: true,
         nav: false,
         dots: true,
-        // autoplay: true
+        autoplay: true
     })
 
     $('.rv-marken-slider').owlCarousel({
@@ -71,3 +101,5 @@ $(document).ready(function () {
 
 
 });
+
+

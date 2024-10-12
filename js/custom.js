f4f9fbb79accb92f005ee67940f070009e6b7279
Md Shakil Hossin <shakilshd23@gmail.com>
2024-10-12 08:18:53 +0600

    Homepage done

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,73 @@
-$('.besten-angebote-slider').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:3
+
+$(document).ready(function () {
+
+    // 
+    $('.besten-angebote-slider').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        responsive: {
+            320: {
+                items: 1
+            },
+            576: {
+                items: 1.5,
+            },
+            768: {
+                items: 2.5,
+            },
+            992: {
+                items: 3,
+            },
+            1200: {
+                items: 2.5,
+            },
+            1400: {
+                items: 3
+            }
         }
-    }
-})
+    })
 
-$('.sonderangebot-slider').owlCarousel({
-    items: 1,
-    loop:true,
-    nav:true,
-    
-})
+    $('.sonderangebot-slider').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
 
-$('.nolan-slider').owlCarousel({
-    items: 1,
-    loop:true,
-    nav:false,
-    dots: true,
-    // autoplay: true
-})
+    })
 
-$('.rv-marken-slider').owlCarousel({
-    items: 6,
-    loop:true,
-    nav:true,
-    autoplay: true,
-    dots: false
-})
+    $('.nolan-slider').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        dots: true,
+        // autoplay: true
+    })
 
+    $('.rv-marken-slider').owlCarousel({
+        items: 6,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        dots: false,
+        responsive: {
+            320: {
+                items: 2
+            },
+            576: {
+                items: 3
+            },
+            768: {
+                items: 4
+            },
+            992: {
+                items: 5
+            },
+            1200: {
+                items: 6
+            }
+        }
+
+    })
+
+
+});

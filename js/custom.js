cc48e107a5360f92615e07bfa296989efe9c5ca0
Md Shakil Hossin <shakilshd23@gmail.com>
2024-10-14 02:37:41 +0600

    Blog Page Done

--- js/custom.js
+++ js/custom.js
@@ -144,6 +144,24 @@ $(document).ready(function () {
         }
     })
 
+    $('.mobile-view').owlCarousel({
+        loop: true,
+        nav: true,
+        // autoplay: true,
+        dots: false,
+        responsive: {
+            320: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            }
+        }
+    })
+
 
 });
 
